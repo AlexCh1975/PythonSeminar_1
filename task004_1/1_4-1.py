15d50ad931919b98d2d1acf880bd4a409b943c20
AlexCh <stovertikal@mail.ru>
2022-09-23 19:06:34 +0700

    add task004_1

---
+++ task004_1/1_4-1.py
@@ -0,0 +1,11 @@
+# Напишите программу, которая будет принимать на вход дробь
+# и показывать первую цифру дробной части числа.
+
+# in 6.78 out 7; in 0.34 out 3; in 5 out no
+
+number = float(input("Введите число: "))
+
+if int(number) == float(number): print("NO")
+else:
+    number = int(number * 10 % 10)
+    print(number)
